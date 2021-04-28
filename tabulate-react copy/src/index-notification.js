5250484762f64da8b6feb8c0a56a1823460a921a
colinfindley <62814733+colinfindley@users.noreply.github.com>
2021-04-28 15:46:36 -0500

    Delete tabulate-react copy directory

--- tabulate-react copy/src/index-notification.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import Notification from './components/Notification.js';
-
-render(<Notification />, document.querySelector('#notification'));
